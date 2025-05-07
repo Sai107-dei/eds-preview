242555c087d5e93e1bcaf3cdbb674ed5ab02ea5e
SaiManikantha <ssanagavarpu@deloitte.com>
2025-05-07 17:44:25 +0530

    .

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,19 +1,12 @@
-// add delayed functionality here
-window.adobeDataLayer = window.adobeDataLayer || []
+window.adobeDataLayer = window.adobeDataLayer || [];
 const metaDescription = document.querySelector('meta[name="description"]').getAttribute('content');
 const pageUrl = document.querySelector('meta[name="canonical"]').getAttribute('content');
 const htmlLang = document.documentElement.getAttribute('lang');
-console.log('Adobe datalayer props' + metaDescription + pageUrl + htmlLang);
 adobeDataLayer.push({
-    "event": "page_view",
-    "pageInfo": {
+    'event': 'page_view',
+    'pageInfo': {
         "pageName": metaDescription,
         "pageURL": pageUrl,
-        "siteSection": "<L1 of site hierarchy>",
-        "siteSubSection1": "<L2 of site hierarchy>",
-        "siteSubSection2": "<L3 of site hierarchy>",
-        "previousPageName": "<Previous Page Name>",
-        "environment": "<Environment of the website>",
         "businessCountryLanguage": htmlLang
 
     }
