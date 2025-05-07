c615af8d14ccc166e7ed5583373e4e2bc43cba97
SaiManikantha <ssanagavarpu@deloitte.com>
2025-05-07 22:13:14 +0530

    .

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,8 +1,8 @@
 window.adobeDataLayer = window.adobeDataLayer || [];
 const { adobeDataLayer } = window;
 const htmlLang = document.documentElement?.getAttribute('lang');
-let canonicalMeta = document.querySelector('meta[name="canonical"]');
-let pageUrl = canonicalMeta ? canonicalMeta.content : null;
+const canonicalMeta = document.querySelector('meta[name="canonical"]');
+const pageUrl = canonicalMeta ? canonicalMeta.content : null;
 adobeDataLayer.push({
   event: 'page_view',
   pageInfo: {
