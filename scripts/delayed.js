a1ce5a484c1cfd0f8faa53e6d5aeb77c1cddd1c3
SaiManikantha <ssanagavarpu@deloitte.com>
2025-05-13 17:52:10 +0530

    .

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -3,29 +3,28 @@ const { adobeDataLayer } = window;
 const htmlLang = document.documentElement?.getAttribute('lang');
 const href = window.location.href;
 let environment;
-if (href.startsWith("https://qa--ewi-lilly-com-block-library-qa--elilillyco.aem.page") || href.includes("https://qa--ewi-lilly-com-block-library-qa--elilillyco.aem.page")) {
-    environment = "QA";
-} else if (href.startsWith("https://dev--ewi-lilly-com-block-library--elilillyco.aem.page") || href.includes("https://dev--ewi-lilly-com-block-library--elilillyco.aem.page")) {
-    environment = "DEV";
+if (href.startsWith('https://qa--ewi-lilly-com-block-library-qa--elilillyco.aem.page') || href.includes('https://qa--ewi-lilly-com-block-library-qa--elilillyco.aem.page')) {
+    environment = 'QA';
+} else if (href.startsWith('https://dev--ewi-lilly-com-block-library--elilillyco.aem.page') || href.includes('https://dev--ewi-lilly-com-block-library--elilillyco.aem.page')) {
+    environment = 'DEV';
 } else {
-    environment = "Unknown Environment";
+    environment = 'Unknown Environment';
 }
 const userAgent = navigator.userAgent || navigator.vendor || window.opera;
 let deviceType; 
 if (/android/i.test(userAgent)) {
-    deviceType = "Android";
+    deviceType = 'Android';
 } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    deviceType = "iOS";
+    deviceType = 'iOS';
 } else if (/Macintosh/i.test(userAgent)) {
-    deviceType = "Mac";
+    deviceType = 'Mac';
 } else if (/Windows/i.test(userAgent)) {
-    deviceType = "Windows";
+    deviceType = 'Windows';
 } else if (/Linux/i.test(userAgent)) {
-    deviceType = "Linux";
+    deviceType = 'Linux';
 } else {
-    deviceType = "Unknown";
+    deviceType = 'Unknown';
 }
-console.log("Device Type:", deviceType);
 let browserType;
 if (/Chrome/i.test(userAgent) && !/Edge|Edg/i.test(userAgent)) {
     browserType = "Chrome";
