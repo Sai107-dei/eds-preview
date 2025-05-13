858a5df9edbc5aed0fcc5398af1c5d2937427095
SaiManikantha <ssanagavarpu@deloitte.com>
2025-05-13 16:47:19 +0530

    .

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,11 +1,21 @@
 window.adobeDataLayer = window.adobeDataLayer || [];
 const { adobeDataLayer } = window;
 const htmlLang = document.documentElement?.getAttribute('lang');
+const href = window.location.href;
+    let environment;
+    if (href.startsWith("https://qa--ewi-lilly-com-block-library-qa--elilillyco.aem.page") || href.includes("https://qa--ewi-lilly-com-block-library-qa--elilillyco.aem.page")) {
+        environment = "QA";
+    } else if (href.startsWith("https://dev--ewi-lilly-com-block-library--elilillyco.aem.page") || href.includes("https://dev--ewi-lilly-com-block-library--elilillyco.aem.page")) {
+        environment = "DEV";
+    } else {
+        environment = "Unknown Environment";
+    }
 adobeDataLayer.push({
   event: 'page_view',
   pageInfo: {
     pageName: document.title,
     pageURL: window.location.href,
+    environment: environment,
     businessCountryLanguage: htmlLang,
   },
 });
