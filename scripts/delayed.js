99f58a8d98e2d9e95e48ae890b6dcdb3398778bf
SaiManikantha <ssanagavarpu@deloitte.com>
2025-05-13 17:54:40 +0530

    .

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,36 +4,36 @@ const htmlLang = document.documentElement?.getAttribute('lang');
 const href = window.location.href;
 let environment;
 if (href.startsWith('https://qa--ewi-lilly-com-block-library-qa--elilillyco.aem.page') || href.includes('https://qa--ewi-lilly-com-block-library-qa--elilillyco.aem.page')) {
-    environment = 'QA';
+  environment = 'QA';
 } else if (href.startsWith('https://dev--ewi-lilly-com-block-library--elilillyco.aem.page') || href.includes('https://dev--ewi-lilly-com-block-library--elilillyco.aem.page')) {
-    environment = 'DEV';
+  environment = 'DEV';
 } else {
-    environment = 'Unknown Environment';
+  environment = 'Unknown Environment';
 }
 const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-let deviceType; 
+let deviceType;
 if (/android/i.test(userAgent)) {
-    deviceType = 'Android';
+  deviceType = 'Android';
 } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    deviceType = 'iOS';
+  deviceType = 'iOS';
 } else if (/Macintosh/i.test(userAgent)) {
-    deviceType = 'Mac';
+  deviceType = 'Mac';
 } else if (/Windows/i.test(userAgent)) {
-    deviceType = 'Windows';
+  deviceType = 'Windows';
 } else if (/Linux/i.test(userAgent)) {
-    deviceType = 'Linux';
+  deviceType = 'Linux';
 } else {
-    deviceType = 'Unknown';
+  deviceType = 'Unknown';
 }
 let browserType;
 if (/Chrome/i.test(userAgent) && !/Edge|Edg/i.test(userAgent)) {
-    browserType = "Chrome";
+  browserType = "Chrome";
 } else if (/Safari/i.test(userAgent) && !/Chrome/i.test(userAgent)) {
-    browserType = "Safari";
+  browserType = "Safari";
 } else if (/Edge|Edg/i.test(userAgent)) {
-    browserType = "Edge";
+  browserType = "Edge";
 } else {
-    browserType = "Unknown Browser";
+  browserType = "Unknown Browser";
 }
 adobeDataLayer.push({
     event: 'page_view',
