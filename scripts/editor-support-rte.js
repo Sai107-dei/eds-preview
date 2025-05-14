581a19d0448a3569c322342ecb97f5c74e889a06
SaiManikantha <ssanagavarpu@deloitte.com>
2025-05-14 17:55:05 +0530

    .

--- scripts/editor-support-rte.js
+++ scripts/editor-support-rte.js
@@ -139,65 +139,6 @@ export function decorateRichtext(container = document) {
     }
   }
 }
-tinymce.PluginManager.add('mycustom', function (editor) {
-
-  editor.ui.registry.addMenuButton('mycustom', {
-
-      text: 'MyCustom',
-
-      fetch: function (callback) {
-
-          callback([
-
-              {
-
-                  type: 'menuitem',
-
-                  text: 'Cust1',
-
-                  onAction: function () {
-
-                      editor.insertContent('<span>Cust1</span>');
-
-                  }
-
-              },
-
-              {
-
-                  type: 'menuitem',
-
-                  text: 'Cust2',
-
-                  onAction: function () {
-
-                      editor.insertContent('<span>Cust2</span>');
-
-                  }
-
-              },
-
-              {
-
-                  type: 'menuitem',
-
-                  text: 'Cust3',
-
-                  onAction: function () {
-
-                      editor.insertContent('<span>Cust3</span>');
-
-                  }
-
-              }
-
-          ]);
-
-      }
-
-  });
-
-});
 
 // in cases where the block decoration is not done in one synchronous iteration we need to listen
 // for new richtext-instrumented elements
