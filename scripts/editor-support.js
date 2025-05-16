2db3e976de7a97abc5130643237376697b164dff
SaiManikantha <ssanagavarpu@deloitte.com>
2025-05-16 16:12:51 +0530

    .

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -13,7 +13,7 @@ import { decorateMain } from './scripts.js';
 function addMetaTag() {
   const meta = document.createElement('meta');
   meta.name = "urn:adobe:aue:config:disable";
-  meta.content = "delete";
+  meta.content = "Delete";
   document.head.appendChild(meta);
  
 }
