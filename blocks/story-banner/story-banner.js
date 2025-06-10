312d9b2fbd003a6e93ca94558e4b514eec596808
Sai107-dei <saimanikantha06@gmail.com>
2025-06-10 22:25:54 +0530

    Update story-banner.js

--- blocks/story-banner/story-banner.js
+++ blocks/story-banner/story-banner.js
@@ -1,24 +1,15 @@
 export default function decorate(block) {
-  const getTrimmedText = (el) => el?.textContent?.trim() || '';
  
-  const [title] = block.children;
-  block.textContent = '';
- 
-  if (title && getTrimmedText(title)) {
-    const h2 = document.createElement('h2');
-    h2.className = 'fs-normal lds-ringside-heading-2';
-    h2.id = getTrimmedText(title);
-    h2.textContent = getTrimmedText(title);
-    h2.setAttribute('aria-label', h2.textContent);
- 
-    let meta = document.querySelector('meta[name="story-title"]');
-    if (!meta) {
-      meta = document.createElement('meta');
-      meta.setAttribute('name', 'story-title');
-      document.head.appendChild(meta);
-    }
-    meta.setAttribute('content', h2.textContent);
- 
-    block.appendChild(h2);
-  }
+const cards = document.createElement('div');
+cards.className = 'cards';
+
+const childDiv = document.createElement('div');
+cards.appendChild(childDiv);
+
+const grandChildDiv = document.createElement('div');
+grandChildDiv.textContent = 'text';
+
+childDiv.appendChild(grandChildDiv);
+
+block.appendChild(cards);
 }
